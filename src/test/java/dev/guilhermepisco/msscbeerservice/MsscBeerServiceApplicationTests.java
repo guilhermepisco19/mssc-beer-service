2d69f73cfc61cad518a882c660aed6353492441a
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2023-07-13 14:51:19 +0100

    Initial project add

---
+++ src/test/java/dev/guilhermepisco/msscbeerservice/MsscBeerServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.guilhermepisco.msscbeerservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsscBeerServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
