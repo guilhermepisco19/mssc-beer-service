1ed65ea64be1c24cc5fa9514ff092dd11e6ce21c
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2023-07-13 15:16:14 +0100

    Adding Beer REST controller

---
+++ src/main/java/dev/guilhermepisco/msscbeerservice/web/controller/BeerController.java
@@ -0,0 +1,35 @@
+package dev.guilhermepisco.msscbeerservice.web.controller;
+
+import dev.guilhermepisco.msscbeerservice.web.model.BeerDto;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
+
+import java.net.URI;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/api/v1/beer")
+public class BeerController {
+
+    @GetMapping("/{beerId}")
+    public ResponseEntity<BeerDto> getBeerById(@PathVariable UUID beerId){
+        //todo impl
+        return ResponseEntity.ok().body(BeerDto.builder().build());
+    }
+
+    @PostMapping
+    public ResponseEntity<Void> saveNewBeer(@RequestBody BeerDto beerDto){
+        //todo impl
+
+        URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
+                .path("/{id}").buildAndExpand(beerDto.getId()).toUri();
+        return ResponseEntity.created(uri).build();
+    }
+
+    @PutMapping("/{beerId}")
+    public ResponseEntity<Void> saveNewBeer(@PathVariable UUID beerId, @RequestBody BeerDto beerDto){
+        //todo impl
+        return ResponseEntity.noContent().build();
+    }
+}
