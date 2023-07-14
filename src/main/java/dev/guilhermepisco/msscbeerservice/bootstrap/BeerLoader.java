0b3282bea797bc15de7403464e45eaf2190695c4
guilhermepisco19 <g.pisco@campus.fct.unl.pt>
2023-07-14 14:23:04 +0100

    Adding bootstrap

---
+++ src/main/java/dev/guilhermepisco/msscbeerservice/bootstrap/BeerLoader.java
@@ -0,0 +1,48 @@
+package dev.guilhermepisco.msscbeerservice.bootstrap;
+
+import dev.guilhermepisco.msscbeerservice.domain.Beer;
+import dev.guilhermepisco.msscbeerservice.repositories.BeerRepository;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+public class BeerLoader implements CommandLineRunner {
+
+    private final BeerRepository beerRepository;
+
+    public BeerLoader(BeerRepository beerRepository) {
+        this.beerRepository = beerRepository;
+    }
+
+
+    @Override
+    public void run(String... args) throws Exception {
+        loadBeerObjects();
+    }
+
+    private void loadBeerObjects() {
+        if(beerRepository.count() == 0){
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Mango Bobs")
+                    .beerStyle("IPA")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(10000L)
+                    .price(BigDecimal.valueOf(12.95))
+                    .build());
+
+            beerRepository.save(Beer.builder()
+                    .beerName("Galaxy Cat")
+                    .beerStyle("PALE_ALE")
+                    .quantityToBrew(200)
+                    .minOnHand(12)
+                    .upc(10001L)
+                    .price(BigDecimal.valueOf(11.95))
+                    .build());
+        }
+
+    }
+}
